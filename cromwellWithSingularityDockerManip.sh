9c6e6cd74756133664ea90b9a70f34b6e85be7a5
Grady King <WVGman2004@icloud.com>
2026-07-08 12:51:46 -0400

    added files i forgot to add

---
+++ cromwellWithSingularityDockerManip.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# used by Cromwell to find the singularity container that corresponds to the Docker container request
+
+docker="$1"
+cwd="$2"
+docker_cwd="$3"
+job_shell="$4"
+docker_script="$5"
+
+# If no docker image is specified, run locally
+if [[ -z "${docker}" ]]; then
+  echo "No docker image specified — running locally."
+  bash "${job_shell}" "${docker_script}"
+  exit $?
+fi
+
+# manipulate quay.io/broadinstitute/viral-core@sha256:1f1b274195ac4f4c7b08c462757b3f124c3cc53f279ce99b938c520a0c5871f3 into viral-core
+docker_base="$(basename "${docker%%[@:]*}")"
+
+if [[ ! -f "/scratch/gpk00003/broadDockers/${docker_base}.sif" ]]; then
+  echo "ERROR: Singularity image /scratch/gpk00003/broadDockers/${docker_base}.sif not found."
+  exit 1
+fi
+
+singularity exec --containall --bind "${cwd}:${docker_cwd}" "/scratch/gpk00003/broadDockers/${docker_base}.sif" "${job_shell}" "${docker_script}"
